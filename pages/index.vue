afa143ef36966d32436798a249f07d3e5a509786
Natchanon Wangpruek <lnwnut227@gmail.com>
2023-08-20 21:41:16 +0700

    Update Root

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="IndexPage">
+        Index
+    </div>
+</template>
+<style scoped>
+    .IndexPage {
+        width: 100vw;
+        text-align: center;
+    }
+</style>
